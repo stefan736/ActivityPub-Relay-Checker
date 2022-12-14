1b0769e4fd77586150700841f77eb113ca153fef
Stefan Berger <stefan@stefanberger.net>
2022-12-14 20:10:05 +0100

    finalized process_like_relay_fedinet_social

--- check.sh
+++ check.sh
@@ -5,7 +5,7 @@ process_like_relay_fedinet_social () {
     contentFile=$3
 
     relayName=$(echo $2 | cut -d '/' -f3)
-    numberOfInstances=$(grep -e 'List of [0-9]* registered instances:' $contentFile | grep -e '[0-9]*' -o -m 1 | head -1)
+    numberOfInstances=$(grep -e '[0-9]* registered instances:' $contentFile | grep -e '[0-9]*' -o -m 1 | head -1)
 
     eval "$1='${relayName};${numberOfInstances}'"
 }
@@ -14,11 +14,11 @@ while read eachRelayURL
 do           
     echo "checking $eachRelayURL ..." 
     data=''
-    #curl -sL "$eachRelayURL" -o content.tmp
-    implMarker=$(xmllint --html --xpath 'string(//p)' content.tmp)
+    curl -sL "$eachRelayURL" -o content.tmp
+    implMarker=$(xmllint --html --xpath 'string(//p)' content.tmp 2>/dev/null)
     if [[ "$implMarker" == "This is an Activity Relay for fediverse instances." ]]; then
         process_like_relay_fedinet_social data $eachRelayURL content.tmp
     fi
     echo $data
-    #rm content.tmp
+    rm content.tmp
 done < relays.txt
